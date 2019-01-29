b9b270399c1fe632a1e0249e24653e85c5fb7d5b
David Martinez <dmartinez@bancoconsorcio.cl>
2019-01-29 11:11:39 -0300

    cards users x logo x edits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private swUpdate: SwUpdate) {
+  }
   title = 'app';
 
   jaja = "jaja"
 
+  ngOnInit() {
+    if (this.swUpdate.isEnabled) {
 
+      this.swUpdate.available.subscribe(() => {
+
+          if(confirm("New version available. Load New Version?")) {
+
+              window.location.reload();
+          }
+
+        });
+      }
+
+}
 
 scroll(id) {
 
@@ -18,6 +35,5 @@ scroll(id) {
   const el = document.getElementById(id);
   el.scrollIntoView();
 }
+
 }
-
-
